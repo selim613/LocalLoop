d7b905b1741c9623245e99cd2305f80507955638
Selim Naas <snaas027@uottawa.ca>
2025-05-25 22:02:24 -0400

    Added login page layout

--- app/src/main/java/com/example/localloop/entities/Admin.java
+++ app/src/main/java/com/example/localloop/entities/Admin.java
@@ -2,6 +2,6 @@ package com.example.localloop.entities;
 
 public class Admin extends User {
     public Admin() {
-        super("Admin", "admin", "XPI76SZUqyCjVxgnUjm0", "admin");
+        super("Admin", "admin", "test", "admin");   //change password to "XPI76SZUqyCjVxgnUjm0" later
     }
 }
