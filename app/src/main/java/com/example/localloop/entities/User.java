2390c0adff04e88bfd1fbd2a008fcb6ea927c748
Selim Naas <snaas027@uottawa.ca>
2025-05-25 20:21:47 -0400

    Added a 'User' abstract class

---
+++ app/src/main/java/com/example/localloop/entities/User.java
@@ -0,0 +1,30 @@
+package com.example.localloop.entities;
+
+public abstract class User {
+    protected String name;
+    protected String username;
+    protected String password;
+    protected String role;
+
+    // Constructor class
+    public User(String name, String username, String password, String role) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getters for the attributes
+    public String getName() {
+        return name;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getRole() {
+        return role;
+    }
+}
